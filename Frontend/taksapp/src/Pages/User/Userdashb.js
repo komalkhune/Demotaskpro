6178e41c3cc97a9863aa64b48f7b73d4259b96e4
Komal Khune <144751813+komalkhune@users.noreply.github.com>
2026-06-21 11:21:04 +0530

    Add files via upload

---
+++ Frontend/taksapp/src/Pages/User/Userdashb.js
@@ -0,0 +1,36 @@
+import { Outlet } from "react-router";
+import mystyle from "./Userdashb.module.css"
+import { useRef, useState } from "react";
+import { IoMdClose, IoMdMenu } from "react-icons/io";
+import Usersidebar from "./Usersidebar";
+
+
+
+const Userdashboard=()=>{
+
+
+  
+
+ 
+
+    return(
+        <>
+
+
+        <div className={mystyle.layout}>
+
+      <Usersidebar />
+
+
+        <div className={mystyle.mainContent}>
+          <Outlet />
+        </div>
+       
+
+      {/* </div> */}
+
+       </div>
+     </>
+    )
+}
+export default Userdashboard;
